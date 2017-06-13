f3220462fa88d499fadd6f2c09d4d3f29c32c017
Keith Dahlby <keith@newbo.co>
2017-06-13 13:01:23 -0500

    Use constructor function prototype

--- class-07-oop-constructors-tables/code-demos/object-literal-to-constructor.js
+++ class-07-oop-constructors-tables/code-demos/object-literal-to-constructor.js
@@ -15,21 +15,10 @@ var myStore = {
 
   getRandomCustomerCount: randomCount,
 
-  generateCustomerCountByHour: function() {
-    this.simulatedCookiesPerHour = [
-      // 6:00
-      this.getRandomCustomerCount() * this.cookiesPerHour,
-      // 7:00
-      this.getRandomCustomerCount() * this.cookiesPerHour,
-      // But don't hard-code this repetitive calcultion;
-      // use a loop instead...
-      this.getRandomCustomerCount() * this.cookiesPerHour,
-    ];
-  }
 };
 
-myStore.generateCustomerCountByHour();
-// ^--- `this` inside generateCustomerCountByHour
+// myStore.generateCustomerCountByHour();
+//   ^--- `this` inside generateCustomerCountByHour
 
 /*
 Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
@@ -42,15 +31,32 @@ function CookieStore(name, minCustomerCount, maxCustomerCount, cookiesPerHour, h
   this.min = minCustomerCount || 10;
   this.max = maxCustomerCount || 100;
   this.cookiesPerHour = cookiesPerHour || 10;
-  this.hourToOpen = hourToOpen || 6;
+  this.hourToOpen = hourToOpen || this.defaultHourToOpen;
   this.hourToClose = hourToClose || 21;
 }
 
+CookieStore.prototype.defaultHourToOpen = 6;
+
+CookieStore.prototype.getRandomCustomerCount = randomCount;
+
+CookieStore.prototype.generateCustomerCountByHour = function() {
+  this.simulatedCookiesPerHour = [
+    // 6:00
+    this.getRandomCustomerCount() * this.cookiesPerHour,
+    // 7:00
+    this.getRandomCustomerCount() * this.cookiesPerHour,
+    // But don't hard-code this repetitive calcultion;
+    // use a loop instead...
+    this.getRandomCustomerCount() * this.cookiesPerHour,
+  ];
+};
+
 var anotherStore = new CookieStore('Keith\'s Cookies', 100, 110, 95.2);
+anotherStore.generateCustomerCountByHour();
 
 var myAwesomeStores = [
-  new CookieStore('Uno'),
-  new CookieStore('Dos'),
+  new CookieStore('Uno', 12, 50, 17),
+  new CookieStore('Dos', 15, 20, 75),
   new CookieStore('Tres'),
 ];
 console.log(anotherStore);
